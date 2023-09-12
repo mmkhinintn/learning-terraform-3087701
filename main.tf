797df3212c71367f978a0f2a947209be09abe4b6
mmkhinintn <52817461+mmkhinintn@users.noreply.github.com>
2023-09-12 09:32:07 +0200

    create dev vpc

--- main.tf
+++ main.tf
@@ -16,6 +16,24 @@ data "aws_ami" "app_ami" {
 
 
 
+module "vpc" {
+  source = "terraform-aws-modules/vpc/aws"
+
+  name = "dev"
+  cidr = "10.0.0.0/16"
+
+  azs             = ["us-west-2a", "us-west-2b", "us-west-2c"]
+  public_subnets  = ["10.0.101.0/24", "10.0.102.0/24", "10.0.103.0/24"]
+
+  enable_nat_gateway = true
+
+
+  tags = {
+    Terraform = "true"
+    Environment = "dev"
+  }
+}
+
 
 resource "aws_instance" "blog" {
   ami           = data.aws_ami.app_ami.id
@@ -30,7 +48,8 @@ resource "aws_instance" "blog" {
 module "blog_sg" {
   source  = "terraform-aws-modules/security-group/aws"
   version = "5.1.0"
-  name = "blog_new"
+  name = "blog"
+  vpc_id = module.vpc.public_subnets[0]
 
   ingress_rules       = ["http-80-tcp","https-443-tcp"]
   ingress_cidr_blocks = ["0.0.0.0/0"]
@@ -40,6 +59,7 @@ module "blog_sg" {
 
 
 }
+
 resource "aws_security_group" "blog" {
   name        = "blog"
   description = "Allow https and https in .Allow everything out"
